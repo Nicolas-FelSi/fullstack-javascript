cfb3b6cae2828cfd7c311f0f5365b7b484230d90
nicolas-felsi <nicolasfelipe3702@gmail.com>
2024-10-31 10:12:52 -0300

    entendendo nodejs

--- pratica-node/src/server.js
+++ pratica-node/src/server.js
@@ -1,17 +1,28 @@
 import http from 'node:http'
 
+const users = [];
+
 const server = http.createServer((req, res) => {
     const { method, url } = req;
 
     if (method === 'GET' && url === "/users") {
-        return res.end("Listagem de usuários.");
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users));
     }
 
     if (method === "POST" && url === "/users") {
-        return res.end("Criação de usuários.")
+
+        users.push({
+            id: 1,
+            name: 'Nicolas',
+            email: 'nicolas@gmail.com'
+        });
+
+        return res.writeHead(201).end("Usuario criado com sucesso");
     }
 
-    return res.end("Hello World");
+    return res.writeHead(404).end("Pagina não encontrada");
 })
 
 server.listen(3333);
