684e53dd502e51f346328c0151be5f4cad270b4f
Nicolas Felipe da Silva <nicolasfelipe3702@gmail.com>
2024-11-10 00:55:14 -0300

    .

--- pratica-node/server.js
+++ pratica-node/server.js
@@ -1,1 +1,16 @@
-import { } from 'node:http'
+// import { createServer } from 'node:http'
+
+// const server = createServer((req, res) => {
+//     res.write("Servidor rodando");
+//     res.end();
+// })
+
+// server.listen(3333);
+
+import { fastify } from "fastify"
+
+const server = fastify();
+
+server.listen({
+    port: 3333
+})
