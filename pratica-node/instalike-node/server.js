09e0159875d955c614a59ff4dda06fb04b7acdec
nicolas-felsi <nicolasfelipe3702@gmail.com>
2024-11-25 10:46:34 -0300

    criando api

--- pratica-node/instalike-node/server.js
+++ pratica-node/instalike-node/server.js
@@ -1,11 +1,29 @@
 import express from "express";
 
-const app = express();
+const posts = [
+    { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
+    { id: 2, descricao: "Gato fazendo Yoga", imagem: "https://placecats.com/millie/300/150" },
+    { id: 3, descricao: "Gato fazendo panqueca", imagem: "https://placecats.com/millie/300/150" },
+];
 
-app.get("/api", (req, res) => {
-    res.send("Resposta aqui");
+function buscaPorId(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id);
+    });
+};
+
+const app = express();
+app.use(express.json());
+
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscaPorId(req.params.id);
+    res.status(200).json(posts[index]);
 })
 
 app.listen(3000, () => {
     console.log("Servidor rodando!");
-})
+});
