4222ba3b4b216dbc50cb406939af2e1ccda85c1c
nicolas-felsi <nicolasfelipe3702@gmail.com>
2024-12-03 11:13:10 -0300

    praticanto criação de api

---
+++ pratica-node/api-rest/src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+
+const selecoes = [
+    { id: 1, selecao: 'Brasil', grupo: 'G' },
+    { id: 2, selecao: 'Suíça', grupo: 'G' },
+    { id: 3, selecao: 'Sérvia', grupo: 'G' },
+    { id: 4, selecao: 'Camarões', grupo: 'G' }
+]
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+app.get("/selecoes", (req, res) => {
+    res.status(200).send(selecoes);
+})
+
+export default app;
